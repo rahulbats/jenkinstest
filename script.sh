25d834cacbd729b266f932bf1c0a69fc5c2037e8
Rahul Bhattacharya <rahulbats@gmail.com>
2023-11-15 12:47:17 -0600

    test

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
 IFS=$'\n'      # Change IFS to newline char
 names=($gitdiff)      # split the `names` string into an array by the same name
 IFS=$SAVEIFS   # Restore original IFS
-
+echo "this is inside script"+$names
 
 for name in "${names[@]}"
 do
