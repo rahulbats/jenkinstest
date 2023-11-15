870eabfd740effec6a7a22032c660c88f849b4dc
Rahul Bhattacharya <rahulbats@gmail.com>
2023-11-15 12:43:39 -0600

    test

--- script.sh
+++ script.sh
@@ -5,3 +5,11 @@ IFS=$'\n'      # Change IFS to newline char
 names=($gitdiff)      # split the `names` string into an array by the same name
 IFS=$SAVEIFS   # Restore original IFS
 
+
+for name in "${names[@]}"
+do
+    echo name
+    IFS=$'\t'
+    callVar=(${name})
+    echo "file"+${callVar[0]}+"-"+${callVar[1]}
+done
