d3b8374f1e42b492e4eb9f12a3c80b3f64050a86
Rahul Bhattacharya <rahulbats@gmail.com>
2023-11-16 09:27:07 -0600

    ett

--- connector.py
+++ connector.py
@@ -3,7 +3,12 @@ gitdiff = os.getenv('gitdiff')
 print(gitdiff)
 
 names=gitdiff.split("\n")
+
+
 for name in names:
   print(name)
   file= name.split("\t")
-  print(file[0]+"-"+file[1])
+  try:
+      print(file[0]+"-"+file[1])
+  except:
+     print("exception occured for"+name)
