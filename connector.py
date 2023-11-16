e89f576b0f42d1302461925e49ce4453d7154794
Rahul Bhattacharya <rahulbats@gmail.com>
2023-11-16 12:07:33 -0600

    test

--- connector.py
+++ connector.py
@@ -15,25 +15,25 @@ for name in names:
       if file[1]=='Jenkinsfile' or file[1]=='connector.py' or file[1]=='sc':
          print('continuing for'+file[1])
          continue
-      
-      match file[0]:
-         case 'D': 
+     
+         if file[0]=='D': 
             connectorName = file[1].replace("json","")
             print("deleting connector"+connectorName)
             r=requests.delete(connectorurl+connectorName)
             print(r)
           
-         case _: 
+         elif file[0]=='A' or file[0]=='M': 
             connectorName = file[1].replace(".json","")
             if file[0]=='A': 
                print("creating connector"+connectorName)
             else:
                print("updating connector"+connectorName)
                data = json.load(open(file[1]))   
+               print(data)   
                headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
                r = requests.put(connectorurl+connectorName+"/config", data=json.dumps(data), headers=headers)
-      print(r)  
-      print(data)
+               print(r) 
+       
       
   except Exception as error:
      print(error)
