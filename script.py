3f0e161cdbb69e18b2e70d9b0f5e5f7b19193e89
Rahul Bhattacharya <rahulbats@gmail.com>
2023-11-16 13:58:21 -0600

    test

--- script.py
+++ script.py
@@ -6,16 +6,41 @@ print(gitdiff)
 
 names=gitdiff.split("\n")
 connectorurl = os.getenv('connectorURL')
+restTopicURL = os.getenv('restURL')+"/v3/clusters/"+os.getenv('kafkaClusterID')+"/topics/"
 
 for name in names:
   print(name)
   file= name.split("\t")
   try:
       print(file[0]+"-"+file[1])
-      if "connector-definitions" not in file[1]:
-         print('continuing for'+file[1])
-         continue
-      else:
+      
+      if "topics" in file[1]:
+        topicName = file[1].replace(".json","").replace("topics/","") 
+        if file[0]=='D': 
+            
+            print("deleting topic"+topicName)
+            r=requests.delete(restTopicURL+topicName)
+            print(r)
+          
+        elif file[0]=='A' or file[0]=='M': 
+            
+            if file[0]=='A': 
+               print("creating topic "+topicName)
+            else:
+               print("updating topic "+topicName)
+               jsonFile=open(file[1])
+               jsonstring="{"+jsonFile.read()+"}"
+              
+               jsonstring=jsonstring.format(**os.environ)
+              
+               #data = json.load(jsonstring)  
+               
+               print("final connector json "+jsonstring)   
+               headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+               r = requests.put(restTopicURL+topicName+"/config", data=jsonstring, headers=headers)
+               print(r)    
+               
+      if "connector-definitions" in file[1]:
          connectorName = file[1].replace(".json","").replace("connector-definitions/","")
          if file[0]=='D': 
             
