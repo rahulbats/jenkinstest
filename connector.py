89156702e327c9562247aebaab686db91b4e7724
Rahul Bhattacharya <rahulbats@gmail.com>
2023-11-16 09:56:23 -0600

    test

--- connector.py
+++ connector.py
@@ -12,9 +12,10 @@ for name in names:
   try:
       print(file[0]+"-"+file[1])
       res = requests.get('ttps://stackoverflow.com/questions/260003h36')
-      data = json.load(open('/Users/rahul/cigna/data.json'))
+      data = json.load(open(file[1]))
+      print(data)
       headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
       r = requests.post(connectorurl, data=json.dumps(data), headers=headers)
       print(res) 
-  except:
-     print("exception occured for"+name)
+  except Exception as error:
+     print("exception occured for"+error)
